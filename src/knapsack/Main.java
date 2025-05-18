9634b18abab7eeea65c3e76bb1747e855973e0fb
natpol567 <148222270+natpol567@users.noreply.github.com>
2025-05-18 23:24:24 +0200

    Main.java

--- src/knapsack/Main.java
+++ src/knapsack/Main.java
@@ -1,9 +1,28 @@
-public class Main {
-    public static void main(String[] args) {   // ← dokładna sygnatura
-        System.out.println("Hello! Works fine.");
+package knapsack;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Give number of items: ");
+        int n = sc.nextInt();
+
+        System.out.print("Give seed: ");
+        long seed = sc.nextLong();
+
+        System.out.print("Give knapsack capacity: ");
+        int C = sc.nextInt();
+
+        Problem p = new Problem(n, seed, 1, 10);
+        for (int i = 0; i < n; i++) {
+            Item it = p.getItems().get(i);
+            System.out.printf("No: %d v: %2d w: %2d%n", i, it.value(), it.weight());
         }
+        System.out.println("---------");
+
+        Result r = p.solve(C);
+        System.out.println(r);
     }
 }
